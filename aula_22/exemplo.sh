4ee29a54f179938f29fce36c61910dc91e6e3593
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-03-05 20:01:05 -0300

    Exemplo de uso de variáveis posicionais

---
+++ aula_22/exemplo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Exemplo de como passar parâmetros ao programa pela
+# linha de comandos (Parâmetros posicionais)
+#
+# ./programa.sh p1 p2 p3 p4
+#       ^       ^  ^  ^  ^
+#       $0      $1 $2 $3 $4
+
+echo "O nome do programa no terminal é: $0"
+echo "O nome do programa é: $( basename $0)"
+
+echo "O primeiro parâmetro passado foi: $1"
+echo "O segundo parâmetro passado foi: $2"
+echo "O terceiro parâmetro passado foi: $3"
+echo "O quarto parâmetro passado foi: $4"
+
+exit 0
