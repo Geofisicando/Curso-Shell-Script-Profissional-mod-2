9f41a540fb7ad1ea875f028029f4aafd544e501d
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2020-03-07 23:44:32 -0300

    Exercício laço while read linha, lista de compras

---
+++ aula_30/exemplo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Exercício while, lista de compras.
+# O programa lê a lista de compras do arquivo passado
+# em $1 exibe, soma os valores e informa ao usuário o
+# custo total
+
+ARQUIVO="lista_compras.txt"
+
+SOMA="0"
+
+while read linha
+do
+	PRODUTO=$(echo "$linha" | cut -d":" -f1)
+	PRECO=$(echo "$linha" | cut -d":" -f2)
+	echo "$PRODUTO ${PRECO}R\$"
+
+	SOMA=$(echo "$SOMA+$PRECO" | bc -l)
+
+done < "$ARQUIVO"
+
+echo "******************"
+echo "TOTAL: ${SOMA}R\$"
+
